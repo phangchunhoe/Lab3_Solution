fb61e069c42415484cdabbb81bfb82d5763e93a4
ilyas <mohamed_ilyas_fakrudeen_abdul_salam@sp.edu.sg>
2022-09-21 09:20:33 +0800

    initial lab3 code.

--- Test_Lab3.py
+++ Test_Lab3.py
@@ -28,3 +28,11 @@ def test_bubble_sort_invalid():
     result = Lab3.bubble_sort(input_arr, 3)
 
     assert (result == [])
+
+def test_bubble_sort_less():
+    result = []
+    input_arr = [64, 34, 25, 12, 22]
+
+    result = Lab3.bubble_sort(input_arr, Lab3.SORT_ASCENDING)
+
+    assert(result == 2)
