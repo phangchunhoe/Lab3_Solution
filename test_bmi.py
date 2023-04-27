61b4fa29b789478ed18d914f50c128ebf505ed59
ilyas <mohamed_ilyas_fakrudeen_abdul_salam@sp.edu.sg>
2023-04-27 10:06:11 +0800

    add test_bmi and modify bmi.c

--- test_bmi.py
+++ test_bmi.py
@@ -2,9 +2,17 @@ import Lab2.bmi as bmi
 
 print("Test_bmi")
 
-def Test_bmi_normal_weight():
+def test_bmi_normal_weight():
+    result = bmi.calculate_bmi(1.73, 60)
+    assert(result==0)
 
-def Test_bmi_over_weight():
 
-def Test_bmi_under_weight():
+def test_bmi_over_weight():
+    result = bmi.calculate_bmi(1.73, 85)
+    assert(result==1 )
+
+
+def test_bmi_under_weight():
+    result = bmi.calculate_bmi(1.73, 50)
+    assert(result==-1)
 
